5c48a11ab58d1f6b646504152680f60f78676676
= <=>
2019-01-24 21:57:07 -0500

    Primer Commit- rest server

---
+++ README.md
@@ -0,0 +1,18 @@
+
+
+# Socket-Server
+
+Reconstruir módulos de Node
+```
+nmp install
+```
+Generar el DIST
+```
+tsc -w 
+```
+
+Levantar Servidor
+```
+nodemon dist/
+node dist/
+```
---
+++ routes/router.ts
@@ -0,0 +1,49 @@
+
+import {Router, Request, Response} from 'express';
+
+const router = Router();
+
+
+router.get( '/mensajes',( req: Request, res: Response) => {
+
+    res.json(
+        {
+            ok:true,
+            mensaje: 'Todo bien'
+        });
+    
+});
+
+router.post( '/mensajes',( req: Request, res: Response) => {
+
+
+    const cuerpo = req.body.cuerpo;
+    const de = req.body.de;
+    res.json(
+        {
+            ok:true,
+            mensaje: 'Mensaje Post listo',
+            cuerpo,
+            de
+        });
+    
+});
+
+router.post( '/mensajes/:id',( req: Request, res: Response) => {
+
+
+    const cuerpo = req.body.cuerpo;
+    const de = req.body.de;
+    const id = req.params.id;
+    res.json(
+        {
+            ok:true,
+            mensaje: 'Mensaje Post listo',
+            cuerpo,
+            de,
+            id
+        });
+    
+});
+
+export default router;
